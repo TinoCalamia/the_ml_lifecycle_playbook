8f9f2b0545d681bc84fc23ec8d9d189fa805b5e6
Agostino Calamia <56636702+TinoCalamia@users.noreply.github.com>
2023-01-31 22:04:27 +0100

    Update data_quality_checks.py

--- data_quality_checks/data_quality_checks.py
+++ data_quality_checks/data_quality_checks.py
@@ -1,7 +1,7 @@
 from datetime import datetime
 import numpy as np
 import pandas as pd
-from typing import List, Union
+from typing import List, Union, Dict, Tuple
 
 
 #########################################################
@@ -9,30 +9,31 @@ from typing import List, Union
 #########################################################
 
 # Population stability index (PSI)
-def calculate_psi(expected: pd.Series, actual = pd.Series, buckettype: str ='bins', buckets: int =10, axis: int =0) -> float:
+def check_psi(expected: pd.Series, actual: pd.Series, threshold: float, buckettype: str ='bins', buckets: int =10, axis: int =0) -> float:
     '''Calculate the PSI (population stability index) for a given column.
     Args:
        expected (pandas.Series): Pandas Series of original values
        actual (pandas.Series): Pandas Series of new values, same size as expected
+       threshold (float): The threshold that needs to be met for a successful test.
        buckettype (string): type of strategy for creating buckets, bins splits into even splits, quantiles splits into quantile buckets
        buckets (integer): number of quantiles to use in bucketing variables
        axis (integer): axis by which variables are defined, 0 for vertical, 1 for horizontal
     Returns:
-       psi_values (float): ndarray of psi values for each variable
+       psi_values (tuple): ndarray of psi values for each variable
     Author:
        Matthew Burke
        github.com/mwburke
        worksofchart.com
     '''
 
-    def psi(expected_array: pd.Series, actual_array: pd.Series, buckets: int) -> int:
+    def psi(expected_array: pd.Series, actual_array: pd.Series, buckets: int) -> Tuple[bool, float]:
         '''Calculate the PSI for a single variable
         Args:
            expected_array: numpy array of original values
            actual_array: numpy array of new values, same size as expected
            buckets: number of percentile ranges to bucket the values into
         Returns:
-           psi_value: calculated PSI value
+           A tuple with a boolean flag if the test passed and calculated PSI value
         '''
 
         def scale_range (input, min, max):
@@ -66,7 +67,7 @@ def calculate_psi(expected: pd.Series, actual = pd.Series, buckettype: str ='bin
         generator = (sub_psi(expected_percents[i], actual_percents[i]) for i in range(0, len(expected_percents)))
         psi_value = np.sum(np.fromiter(generator, dtype=float))
 
-        return(psi_value)
+        return( psi_value>psi_value, psi_value )
 
     if len(expected.shape) == 1:
         psi_values = np.empty(len(expected.shape))
@@ -84,7 +85,7 @@ def calculate_psi(expected: pd.Series, actual = pd.Series, buckettype: str ='bin
     return(psi_values)
   
 # Evaluate if the input data has the correct data type for all columns
-def evaluate_data_types(data: pd.DataFrame, column_spec) -> List:
+def check_data_types(data: pd.DataFrame, column_spec) -> Tuple[bool, List]:
     """
     Evaluates if the input data has the correct data types according to the column specification
 
@@ -93,7 +94,7 @@ def evaluate_data_types(data: pd.DataFrame, column_spec) -> List:
     column_spec (dict): The column specification with keys as column names and values as data types
 
     Returns:
-    bool: True if all input data has the correct data type, False otherwise
+     A tuple with a boolean indicating if the test passed and the columns with wrong data types
     """
     wrong_dtype_columns = []
     for column in data.columns:
@@ -107,17 +108,17 @@ def evaluate_data_types(data: pd.DataFrame, column_spec) -> List:
               print(f"Column {column} is not the expected data type. Expected is {column_spec[column]} but is {data[column].dtype}")
               wrong_dtype_columns = wrong_dtype_columns + [column]
     
-    return wrong_dtype_columns 
+    return ( len(wrong_dtype_columns) == 0, wrong_dtype_columns )
   
   
-  def check_for_outliers(data: pd.DataFrame, threshold: Union[dict, float]) -> List:
+def check_outlier_ratio(data: pd.DataFrame, threshold: Union[dict, float]) -> Tuple[bool, List]:
     '''Calculate the ratio of outliers for each column.
     
     Args:
       data (pd.DataFrame): DataFrame containing the data to be checked.
       threshold (Dict | Float): If all columns shall be tested on the same threshold a float is passed. Otherwise a dict needs to be specified with the column name as key and the threshold value as value.
     Returns:
-      high_outlier_columns (list): List containing all columns with an unexpected high ratio of outliers.
+      A tuple with a boolean indicating if the test passed and the columns with a high ratio of outliers.
     '''
     high_outlier_columns = []
     
@@ -148,7 +149,7 @@ def evaluate_data_types(data: pd.DataFrame, column_spec) -> List:
           print(f"Column {column} has too many outliers. A relative amount of {ratio_of_removed_rows} outliers have been observed. Expected were {threshold}.")
           high_outlier_columns = high_outlier_columns + [column]
       
-      return high_outlier_columns
+      return (len(high_outlier_columns) == 0, high_outlier_columns)
   
   
 #########################################################
@@ -156,33 +157,36 @@ def evaluate_data_types(data: pd.DataFrame, column_spec) -> List:
 #########################################################
 
 # Check expected number of missing values
-def calculate_perc_of_missing_values(data: pd.DataFrame, column: string, threshold: float) -> float:
+def check_perc_of_missing_values(data: pd.DataFrame, thresholds: Dict) -> Tuple[bool, List]:
   '''Calculate the relative amount of missing values in a column.
   Args:
     data (pd.DataFrame): DataFrame containing the data that needs to be checked.
-    column (string): Column name that needs to be checked.
-    threshold (float): Threshold set according to business requirements.
+    threshold (Dict): Dictionary containing the columns which are tested as keys and the threshold for missing values to be accepted.
   
   Returns:
-    percent_missing (float): The relative amount of missing values.
+    A tuple with a boolean indicating if the test passed and the columns with a high ratio of missing values.
   
   '''
-  
-  percent_missing = data[column].isnull().sum() / len(data)
-  
-  if percent_missing > threshold:
-    print(f"Column {column} has more than the expected amount of missing values. {percent_missing} is missing, but threshold is {threshold}.")
-  else:
-    print(f"Column {column} has less then the expected amount of missing values.")
+  columns_w_missing_values = []
     
-  return percent_missing
+  for column in thresholds.keys():
+      percent_missing = data[column].isnull().sum() / len(data)
+
+      if percent_missing > thresholds[column]:
+        print(f"Column {column} has more than the expected amount of missing values. {percent_missing} is missing, but threshold is {thresholds[column]}.")
+        
+        columns_w_missing_values = columns_w_missing_values + [column]
+      else:
+        print(f"Column {column} has less then the expected amount of missing values.")
+    
+  return (len(percent_missing) == 0, columns_w_missing_values)
 
 #########################################################
 # 3. Reliability
 #########################################################
 
 # Test Uniqueness
-def check_for_uniqueness(data: pd.DataFrame, no_dup_columns: List) -> List:
+def check_uniqueness(data: pd.DataFrame, no_dup_columns: List) -> Tuple[bool, List]:
   '''Investigate if columns which are expected to have no duplicates, only contain unique values.
   
   Args:
@@ -190,10 +194,7 @@ def check_for_uniqueness(data: pd.DataFrame, no_dup_columns: List) -> List:
     no_dup_columns (List): List of columns for which no duplication is expected.
     
   Returns:
-    columns_with_dups (List): A list of columns that contain duplicates even though not expected.
-  
-  
-  
+    A tuple with a boolean indicating if the test passed and the columns with duplicated values.
   '''
   columns_with_dups = []
   
@@ -202,21 +203,21 @@ def check_for_uniqueness(data: pd.DataFrame, no_dup_columns: List) -> List:
       print(f" Column {column} is expected to not have any duplications. {data[column].nunique()} unique values are observed but the data contains {len(data)} entries.")
       columns_with_dups + [column]
             
-  return columns_with_dups
+  return (len(columns_with_dups) == 0, columns_with_dups)
 
 
 #########################################################
 # 4. Timelineness
 #########################################################
 
-def check_data_freshness(data: pd.DataFrame, thresholds: Dict) -> Dict:
+def check_data_freshness(data: pd.DataFrame, thresholds: Dict) -> Tuple[bool, Dict]:
   '''Check if all columns with dates are fresh.
   Args:
     data (pd.DataFrame): DataFrame containing the data that needs to be checked.
     thresholds (Dict({string : int})): The keys represent the column and the value the number of days that the most recent entry should not exceed.
   
   Returns:
-    outdated_columns (Dict({string : int})): All columns with the most recent date past the threshold
+        A tuple with a boolean indicating if the test passed and the columns with outdate columns.
   '''
 
   outdated_columns = {}
@@ -232,6 +233,4 @@ def check_data_freshness(data: pd.DataFrame, thresholds: Dict) -> Dict:
       print(f"Column {column} has outdated data. The most recent entry is {days_diff} days old but {thresholds[column]} is maximum accepted.")
       outdated_columns[column] = days_diff
 
-  return outdated_columns
-    
-
+  return ( len(outdated_columns.keys())==0, outdated_columns)
